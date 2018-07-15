b7788eedf619fba5978528e94359a113310c475b
Joseph Parker <jparker010190@gmail.com>
2018-07-15 02:44:09 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,15 +27,10 @@ class SongsController < ApplicationController
   end
 
   def update
-    @song = Song.find(params[:id])
-
-    @song.update(song_params)
-
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
+    CSV.foreach(params[:leads].path, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
+    redirect_to customers_path
   end
 
   def destroy
