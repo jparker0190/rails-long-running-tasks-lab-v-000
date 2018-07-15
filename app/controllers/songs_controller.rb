1d33a7eb42c608ed91cf0aeec16719b62ac41f08
Joseph Parker <jparker010190@gmail.com>
2018-07-15 02:48:09 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -30,7 +30,7 @@ class SongsController < ApplicationController
     CSV.foreach(params[:leads].path, headers: true) do |lead|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
-    redirect_to artists_path
+    redirect_to songs_path
   end
 
   def destroy
@@ -46,3 +46,4 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
+z
